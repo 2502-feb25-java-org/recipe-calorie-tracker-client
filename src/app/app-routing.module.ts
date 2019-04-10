d34d86a9a22ec1d96d1944a081e623d809c97adf
Amro Arafat <amroarafat84@gmail.com>
2019-04-10 13:26:40 -0400

    Adding nav-bar with route to register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserregisterComponent } from './components/userregister/userregister.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: UserregisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
