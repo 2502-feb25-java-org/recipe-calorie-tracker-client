383399fd6716c4ed2931884085b50a6375d711ea
Amro Arafat <amroarafat84@gmail.com>
2019-04-10 14:41:34 -0400

    modify login functionality

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { UserNamePassword } from 'src/app/models/user-name-password';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user:UserNamePassword = new UserNamePassword();
+  message:string = "";
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
   }
+
+  submit(): void {
+    this.userService.login(this.user)
+        .subscribe( data => {
+          console.log(data);
+          if(data != null)
+              this.router.navigateByUrl('/dashboard');
+          else{
+              this.message = "Wrong Credential"
+              }
+        });
+
+  };
 
 }
