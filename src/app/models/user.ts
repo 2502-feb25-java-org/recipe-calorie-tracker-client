5f99e2459d220f24f7f20f42a5ef69f644a7d968
Amro Arafat <amroarafat84@gmail.com>
2019-04-10 12:53:11 -0400

    Adding user register

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    firstName:string;
+    lastName:string;
+    username:string;
+    password:string;
+    age:number;
+    weight:number;
+    height:number;
+    maxCalorieIntake:number;
+    
+}
