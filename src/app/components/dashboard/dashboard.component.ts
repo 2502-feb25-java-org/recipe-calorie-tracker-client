cfd65c5050c87b6e5bff0f43eeab3c363ceacf0e
alexander <a.v.shushunov@gmail.com>
2019-04-12 10:44:52 -0400

    datatable ingredients in dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IngredientService } from 'src/app/services/ingredient.service';
+import { DataTablesModule } from 'angular-datatables';
+import { Ingredient } from '../../models/ingredient';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,13 +12,33 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private router:Router) { 
+  public ingredients: Ingredient[] = [];
+  public dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<void> = new Subject();
+
+  constructor(private _service: IngredientService) { 
 
   }
   ngOnInit() {
+    /*
     if(sessionStorage.getItem("user") == null){
       this.router.navigateByUrl('');
-    }
+    }*/
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10
+    };
+    this._service.getIngredients().subscribe( data => {
+      this.ingredients = data;
+      this.dtTrigger.next();
+      //console.log(this.ingredients);
+      //let ingredients = JSON.parse(data);
+      //console.log(ingredients);
+      //console.log(data);
+      //let obj = JSON.stringify;
+      //console.log(obj);
+      //console.log(obj[0]);
+     });
   }
 
 }
